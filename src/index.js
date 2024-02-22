a46b3a1b3b57085408a34fae91f03d9aaf6fd6a6
gaurav <gaurav30012000jha@gmail.com>
2024-02-22 18:57:58 +0530

    Added token comaprison logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { PORT } = require('./config/serverConfig')
   const app = express();
 const apiRoutes = require('./routes/index');
 
+const UserService = require('./services/user-service');
+
 const { User } = require('./models/index');
 const bcrypt = require('bcrypt');
 const UserRepository = require('./repository/user-repository');
@@ -19,10 +21,15 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        const repo = new UserRepository();
-        const response =await repo.getById(3);
-        console.log(response.email);
-        //   
+        // const repo = new UserRepository();
+        // const response =await repo.getById(3);
+        // console.log(response.email);
+        const service = new UserService();
+        const newToken = service.createToken({email: 'cvent@admin.com', id: 1});
+        console.log('new token is', {newToken} );
+        const response = service.verifyToken(newToken);
+        console.log(response);
+        //  
     });
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const UserRepository = require('../repository/user-repository');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService{
     constructor(){
@@ -12,6 +15,33 @@ class UserService{
         } catch (error) {
             console.log("Something went wrong in Service layer");
             throw error;
+        }
+    }
+    createToken(user){
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn : '1h'} );
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in validating token", error);
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+        } catch (error) {
+            console.log("Soemthing went wrong while comparing password refer checkPassword fn");
         }
     }
 }
