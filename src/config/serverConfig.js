7828f4e88d73bab6a9cf4b6dff0d6e151a52f9e8
Gaurav Jha <Gaurav_Jha@epam.com>
2024-07-07 19:54:11 +0530

    made config changes

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -8,8 +8,8 @@ module.exports = {
     SALT : bcrypt.genSaltSync(10),
     JWT_KEY : process.env.JWT_KEY,
     DEV_USERNAME: process.env.DEV_USERNAME,
-     DEV_PASSWORD: process.env.DEV_PASSWORD,
-     DEV_DATABASE: process.env.DEV_DATABASE,
-     DEV_HOST: process.env.DEV_HOST,
+    DEV_PASSWORD: process.env.DEV_PASSWORD,
+    DEV_DATABASE: process.env.DEV_DATABASE,
+    DEV_HOST: process.env.DEV_HOST,
     DEV_PORT: process.env.DEV_PORT,
 }
--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 
 const db =  require('./models/index');
-const { User, Role } =  require('./models/index'); 
-
-//const { User } = require('./models/index');
-const bcrypt = require('bcrypt');
-
 
 const prepareAndStartServer = () => {
 
@@ -21,22 +16,6 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        // if(process.env.DB_SYNC){
-        //   db.sequelize.sync({alter: true});
-        // }
-
-        // const u1 = await User.findByPk(11);
-        // const r1 = await Role.findByPk(1);
-        // u1.addRole(r1);
-        // const repo = new UserRepository();
-        // const response =await repo.getById(3);
-        // console.log(response.email);
-        // const service = new UserService();
-        // const newToken = service.createToken({email: 'cvent@admin.com', id: 1});
-        // console.log('new token is', {newToken} );
-        // const response = service.verifyToken(newToken);
-        // console.log(response);
-        //  
     });
 }
 
